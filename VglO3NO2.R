e6298a6ee29bf8c98cebb63a8a14c112fb738b84
Matthias Lochmann <59730731+Ma-Loma@users.noreply.github.com>
2025-03-05 10:19:25 +0100

    Jetzt bunt

--- VglO3NO2.R
+++ VglO3NO2.R
@@ -1,22 +1,24 @@
 library(tidyverse)
 library(readr)
+library(viridis)
 
 no2 <- read_delim(
   "data/no2.txt",
   delim = ";",
   escape_double = FALSE,
-  trim_ws = TRUE
+  trim_ws = TRUE,
+  locale = locale(decimal_mark = ",")
 ) %>%
   select(where(function(x)
     ! all(is.na(x)))) %>% 
   mutate(Stoff="NO2")
 
-
 o3 <- read_delim(
   "data/o3.txt",
   delim = ";",
   escape_double = FALSE,
-  trim_ws = TRUE
+  trim_ws = TRUE,
+  locale = locale(decimal_mark = ",")
 ) %>%
   select(where(function(x)
     ! all(is.na(x)))) %>% 
@@ -24,20 +26,27 @@ o3 <- read_delim(
 
 beide <- bind_rows(no2, o3) %>% 
   pivot_longer(-c(Station, Stoff), names_to="Jahr", values_to="Wert")%>% 
-  filter(!is.na(Wert))
-
-
-
-beide %>% 
-  ggplot(aes(x=Jahr, y=Wert, group=Station,color=Stoff))+
-  geom_point()
+  filter(!is.na(Wert)) %>% 
+  mutate(Jahr=as.numeric(Jahr))
 
 beide %>% 
-  pivot_wider(names_from=Stoff, values_from=Wert) %>%
-  ggplot(aes(x=NO2, y=O3,color=Station)) + 
+  ggplot(aes(x=Jahr, y=Wert, group=Station))+
+  geom_line(aes(group=Station))+
+  facet_wrap(vars(Stoff), scales="free_y")
+
+
+beide %>%
+  pivot_wider(names_from = Stoff, values_from = Wert) %>%
+  ggplot(aes(x = NO2, y = O3)) +
   xlab("NO2 Jahresmittelwert") + ylab("O3 Jahresmittelwert") +
-  geom_line() +
-  coord_cartesian(xlim=c(0,400),ylim = c(0, 850))
+  geom_point(aes(color = factor(Jahr))) +
+  geom_line(aes(color = factor(Jahr), group = Station)) +
+  scale_color_viridis(discrete=TRUE, option="viridis")+
+  coord_cartesian(xlim = c(0, 40), ylim = c(25, 85))
 
-ggsave("O3_NO2_Korrelation.png", width=20, height=10, dpi=300)
-  
+ggsave(
+  "O3_NO2_Korrelation.png",
+  width = 20,
+  height = 10,
+  dpi = 300
+)
